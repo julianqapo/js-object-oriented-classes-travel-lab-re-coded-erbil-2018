9fabcdd6de4b8a39087c45f679ad0d3d5b7a9f21
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-16 11:43:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+/*class Driver {
+constructor(name, date){
+  this.name = name;
+  date += "z"
+  this.date = new Date(date)
+}
+
+}
+*/
+
+
+class Driver{
+constructor(name, date){
+this.name = name;
+this.date = new Date;
+
+}
+
+
+}
 
